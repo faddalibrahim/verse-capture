0e72e7e21018e8638f33874c3399e1b391ad2d03
faddalibrahim <faddalibrahim@gmail.com>
2025-02-20 04:48:07 +0000

    feat: create hook for darkmode

---
+++ frontend/src/hooks/useDarkMode.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
+  return { darkMode, toggleDarkMode };
+};
