bb1eb09158ac0a4d2ab30aa64dd825c8728896ae
faddalibrahim <faddalibrahim@gmail.com>
2025-02-20 04:50:28 +0000

    feat: clean App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import Controls from "./components/Controls";
-import BibleQuoteDisplay from "./components/BibleQuoteDisplay";
-import AppStyles from "./css/App.module.css";
-import Icons from "./components/Icons"; // Import Icons
+import BibleQuoteDisplay from "./components/VerseDisplay";
+import Header from "./components/Header";
 
 function App() {
   const [verse, setVerse] = useState<{
@@ -12,38 +11,11 @@ function App() {
     text: string;
   } | null>(null);
 
-  const [darkMode, setDarkMode] = useState(false); // State for dark mode
-
-  const handleTranscript = (text: string) => {
-    console.log(text);
-  };
-
-  const toggleDarkMode = () => {
-    setDarkMode((prevMode) => !prevMode);
-  };
-
   return (
-    <div
-      className={`${AppStyles.app} ${
-        darkMode ? AppStyles.dark : AppStyles.light
-      }`}
-    >
-      <header className={AppStyles.header}>
-        <h1 className={AppStyles.appName}>
-          Verse
-          <Icons.BookOpen />
-          Capture
-        </h1>
-        <button onClick={toggleDarkMode} className={AppStyles.toggleButton}>
-          {darkMode ? <Icons.Sun /> : <Icons.Moon />}
-        </button>
-      </header>
-      <BibleQuoteDisplay verse={verse} darkMode={darkMode} />
-      <Controls
-        onTranscript={handleTranscript}
-        onVerseReceived={setVerse}
-        darkMode={darkMode}
-      />
+    <div className="h-[100dvh] flex flex-col justify-between items-center bg-gray-50 dark:bg-[#1c1b22]/98">
+      <Header />
+      <BibleQuoteDisplay verse={verse} />
+      <Controls onVerseReceived={setVerse} />
     </div>
   );
 }
